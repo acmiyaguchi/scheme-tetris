d9d4a8e598a308be499509c40d9e386b538b07d2
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2013-09-11 23:45:02 -0700

    Added seperate files to handle tetrominoes, function to display the current screen.

---
+++ tetris.scm
@@ -0,0 +1,71 @@
+(declare (block) (fixnum) (usual-integrations))
+
+(use ncurses srfi-25)
+(load "tetromino.scm")
+
+;; Tetris is scheme
+
+(define STARTX 0)
+(define STARTY 0)
+(define ENDX 79)
+(define ENDY 24)
+
+(define CELL_CHAR #\#)
+(define TIME_OUT 300)
+
+(define cols #f)
+(define lines #f)
+
+(define color car)
+(define display? cdr)
+(define color-set! set-car!)
+(define display?-set! set-cdr!)
+
+(define (tetra-display win area startx starty endx endy)
+  (wclear win)
+  (do ([i startx (add1 i)])
+    ((>= i endx))
+    (do ([j starty (add1 j)])
+      ((>= j endy))
+      (unless (not (display? (array-ref area i j)))
+        (mvwaddch win j i CELL_CHAR))))
+  (wrefresh win))
+
+
+
+(define (main)
+  ;;Settings for the terminal session
+  (initscr)
+  (noecho)
+  (cbreak)
+  (curs_set 0)
+  (keypad (stdscr) #t)
+  (timeout TIME_OUT)
+
+  ;;Settings for the window
+  (set! cols (COLS))
+  (set! lines (LINES))
+  (set! ENDX (sub1 cols))
+  (set! ENDY (sub1 lines))
+  (let ([workarea (make-array (shape 0 cols 0 lines))])
+    (do ([i 0 (add1 i)])
+      ((>= i cols))
+      (do([j 0 (add1 j)])
+        ((>= j lines))
+        (array-set! workarea i j (cons 0 #f))))
+        (tetra-display (stdscr) workarea STARTX STARTY ENDX ENDY)
+        (let loop ((continue #t)) ;;Start the main game loop
+          (unless (not continue)
+            (case (getch)
+              ((#\q) (set! continue #f))
+              )
+            (tetra-display (stdscr) workarea STARTX STARTY ENDX ENDY)
+            (loop continue))))
+  (echo)
+  (nocbreak)
+  (curs_set 1)
+  (endwin))
+
+(main)
+
+
---
+++ tetromino.scm
@@ -0,0 +1,43 @@
+;;Defintions and functions regarding tetrominoes
+
+;;Create the basic tetromino block
+(define (make-tetrimino x1 y1 x2 y2 x3 y3 x4 y4)
+  (list (cons x1 y1) (cons x2 y2) (cons x3 y3) (cons x4 y4)))
+
+(define (L-block)
+  (make-tetrimino 0 0 1 0 2 0 3 0))
+
+(define (J-block)
+  (make-tetrimino 0 0 0 1 1 1 2 1))
+
+(define (L-block)
+  (make-tetrimino 0 1 1 1 2 1 2 0))
+
+(define (O-block)
+  (make-tetrimino 0 0 0 1 1 0 1 1))
+
+(define (S-block)
+  (make-tetrimino 1 0 2 0 0 1 1 1))
+
+(define (T-block)
+  (make-tetrimino 1 0 0 1 1 1 2 1))
+
+(define (Z-block)
+  (make-tetrimino 0 0 1 0 1 1 2 1))
+
+
+;;Functions that work on blocks
+
+;; Rotates a block clockwise
+(define (rot-clockwise tetra)
+  (map 
+    (lambda (pair)
+      (list (* (cdr pair) -1) (car pair)))
+    tetra))
+
+;;Rotates a block counterclockwise
+(define (rot-counterclockwise tetra)
+  (map
+    (lambda (pair)
+      (list (cdr pair) (* (car pair) -1)))
+    tetra))
