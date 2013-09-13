88ccbed5acae2e23ac21b010a64f560874f7e08c
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2013-09-12 22:27:58 -0700

    Added a proper rotation facility by isolating offset and base coordinates of tetrominoes

--- tetromino.scm
+++ tetromino.scm
@@ -1,48 +1,69 @@
 ;;Defintions and functions regarding tetrominoes
 
+(use ncurses)
+
+;; caar -> color
+;; cdar -> offset pair
+;; cadr -> coords
+(define block-color caar)
+(define block-offset cdar)
+(define block-coords cadr)
+
 ;;Create the basic tetromino block
-(define (make-tetrimino x1 y1 x2 y2 x3 y3 x4 y4)
-  (list (cons x1 y1) (cons x2 y2) (cons x3 y3) (cons x4 y4)))
+(define (make-tetrimino color x1 y1 x2 y2 x3 y3 x4 y4)
+  (list (cons color (cons 0 0)) ;;associated metadata
+        (list (cons x1 y1) 
+              (cons x2 y2) 
+              (cons x3 y3)
+              (cons x4 y4))))
 
 (define (L-block)
-  (make-tetrimino 0 0 1 0 2 0 3 0))
+  (make-tetrimino COLOR_RED 0 0 1 0 2 0 3 0))
 
 (define (J-block)
-  (make-tetrimino 0 0 0 1 1 1 2 1))
+  (make-tetrimino COLOR_GREEN 0 0 0 1 1 1 2 1))
 
 (define (L-block)
-  (make-tetrimino 0 1 1 1 2 1 2 0))
+  (make-tetrimino COLOR_YELLOW 0 1 1 1 2 1 2 0))
 
 (define (O-block)
-  (make-tetrimino 0 0 0 1 1 0 1 1))
+  (make-tetrimino COLOR_BLUE 0 0 0 1 1 0 1 1))
 
 (define (S-block)
-  (make-tetrimino 1 0 2 0 0 1 1 1))
+  (make-tetrimino COLOR_MAGENTA 1 0 2 0 0 1 1 1))
 
 (define (T-block)
-  (make-tetrimino 1 0 0 1 1 1 2 1))
+  (make-tetrimino COLOR_CYAN 1 0 0 1 1 1 2 1))
 
 (define (Z-block)
-  (make-tetrimino 0 0 1 0 1 1 2 1))
+  (make-tetrimino COLOR_WHITE 0 0 1 0 1 1 2 1))
 
-
+;;-------------------------------
 ;;Functions that work on blocks
+;;-------------------------------
+;;Helper function of the rotate series of function
+;;Takes in a block, and returns the coordinate list with subtracted offsets
+(define (calc-offset off ls)
+  (map (lambda (item)
+         (+ (car item) (car off))
+         (+ (cdr item) (cdr off)))
+       ls))
+
+;;Displace the block by modifying the current offsets
+(define (move-block tetra dx dy)
+    (set! (cadar tetra) (+ (cadar tetra) dx))
+    (set! (cddar tetra) (+ (cddar tetra) dy)))
 
 ;; Rotates a block clockwise
-(define (rot-clockwise tetra)
-  (map 
-    (lambda (pair)
-      (cons (* (cdr pair) -1) (car pair)))
-    tetra))
+(define (rot-cw tetra)
+  (cons (car tetra)
+        (map (lambda 
+               (pair)(cons (* (cdr pair) -1) (car pair)))
+             (block-coords tetra))))
 
 ;;Rotates a block counterclockwise
-(define (rot-counterclockwise tetra)
-  (map
-    (lambda (pair)
-      (cons (cdr pair) (* (car pair) -1)))
-    tetra))
-
-(define (move tetra x y)
-  (map (lambda (coord)
-         (cons (+ (car coord) x) (+ (cdr coord) y)))
-       tetra))
+(define (rot-ccw tetra)
+  (cons (car tetra)
+        (map(lambda (pair)
+              (cons (cdr pair) (* (car pair) -1)))
+          (block-coords tetra))))
